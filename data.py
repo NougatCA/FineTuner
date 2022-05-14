2011a11130b652f13efb250e8f24c51b54d939f5
NougatCA <nougatca@outlook.com>
2022-05-14 16:53:18 +0800

    Update changes

--- data.py
+++ data.py
@@ -1,14 +1,16 @@
 import os
 from dataclasses import dataclass
 import json
+from tqdm import tqdm
 
 
 @dataclass
 class ClassificationExample:
-    """Raw example of classification tasks, source_pair is used for input-pair classification."""
+    """Raw example of classification tasks."""
     idx: str
     source: str
     label: int
+    # used for input-pair classification
     source_pair: str = None
 
 
@@ -28,7 +30,31 @@ class Seq2SeqExample:
     target: str
 
 
-def load_examples(args, split, aux_file=None):
+def load_aux_data(args):
+
+    data_dir = os.path.join(args.data_dir, args.task, args.dataset)
+    if args.subset:
+        data_dir = os.path.join(args.data_dir, args.subset)
+
+    # bigclonebench
+    if args.task == "clone":
+        if args.dataset == "bigclonebench":
+            idx_to_code = {}
+            with open(os.path.join(data_dir, "data.jsonl"), mode="r", encoding="utf-8") as f:
+                lines = f.readlines()
+                for line in tqdm(lines, total=len(lines), desc="Loading raw data"):
+                    js = json.loads(line.strip())
+                    code = ' '.join(js['func'].split())
+                    idx_to_code[js['idx']] = code
+            return idx_to_code
+    elif args.task == "exception":
+        with open(os.path.join(data_dir, "types.txt"), mode="r", encoding="utf-8") as f:
+            type_list = f.read().split()
+        return type_list
+    return None
+
+
+def load_examples(args, split, aux_data=None):
 
     data_dir = os.path.join(args.data_dir, args.task, args.dataset)
     if args.subset:
@@ -38,18 +64,39 @@ def load_examples(args, split, aux_file=None):
     if args.task == "defect":
         assert split in ["train", "valid", "test"]
         with open(os.path.join(data_dir, f"{split}.jsonl"), mode="r", encoding="utf-8") as f:
-            for _, line in enumerate(f):
+            lines = f.readlines()
+            for line in tqdm(lines, total=len(lines), desc=f"Loading {split} set"):
                 line = line.strip()
                 js = json.loads(line)
                 code = ' '.join(js['func'].split())
-                examples.append(
-                    ClassificationExample(
-                        idx=js['idx'],
-                        source=code,
-                        label=int(js['target'])
-                    )
-                )
+                examples.append(ClassificationExample(idx=js['idx'],
+                                                      source=code,
+                                                      label=int(js['target'])))
     elif args.task == "clone":
         assert split in ["train", "valid", "test"]
+        assert aux_data     # a dict map from idx to code
         with open(os.path.join(data_dir, f"{split}.txt"), mode="r", encoding="utf-8") as f:
-            for
+            lines = f.readlines()
+            for idx, line in enumerate(tqdm(lines, total=len(lines), desc=f"Loading {split} set")):
+                url1, url2, label = line.strip().split('\t')
+                if url1 not in aux_data or url2 not in aux_data:
+                    continue
+                label = int(label)
+                examples.append(ClassificationExample(idx=str(idx),
+                                                      source=aux_data[url1],
+                                                      source_pair=aux_data[url2],
+                                                      label=label))
+    elif args.task == "exception":
+        assert split in ["train", "valid", "test"]
+        assert aux_data     # a list of all types
+        with open(os.path.join(data_dir, f"{split}.jsonl"), mode="r", encoding="utf-8") as f:
+            lines = f.readlines()
+            for idx, line in enumerate(tqdm(lines, total=len(lines), desc=f"loading {split} set")):
+                js = json.loads(line.strip())
+                code = " ".join(js["function"].split())
+                target_txt = js["label"].lower()
+                examples.append(ClassificationExample(idx=str(idx),
+                                                      source=code,
+                                                      label=aux_data.index(target_txt)))
+
+    elif
--- run_fine_tune.py
+++ run_fine_tune.py
@@ -1,11 +1,15 @@
 
 from models import init_model_tokenizer
-from data import load_examples
+from data import load_examples, load_aux_data
 
 
 def run_fine_tune(args):
 
     model, tokenizer = init_model_tokenizer(args)
+
+    aux_data = None
+    if args.dataset in ["bigclonebench"]:
+        aux_data = load_aux_data()
 
     if args.do_train:
         train_examples = load_examples()
