2ba7301d57f71a5d301a7141c93812b6563a5604
NougatCA <nougatca@outlook.com>
2022-05-23 19:10:08 +0800

    Updates

--- src/models.py
+++ src/models.py
@@ -85,19 +85,19 @@ class RobertaClassificationHead(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        # self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        # classifier_dropout = (
-        #     config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
-        # )
-        # self.dropout = nn.Dropout(classifier_dropout)
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
         self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
     def forward(self, features, **kwargs):
         x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
-        # x = self.dropout(x)
-        # x = self.dense(x)
-        # x = torch.tanh(x)
-        # x = self.dropout(x)
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
         x = self.out_proj(x)
         return x
 
